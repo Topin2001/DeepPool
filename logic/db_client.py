f2f22dda1b4a271aad9d16cf3b312900449012de
Topin2001 <wonodoou00@gmail.com>
2026-04-04 12:19:04 +0200

    Update influxdb version to 2.X.X (latest)

--- logic/db_client.py
+++ logic/db_client.py
@@ -1,16 +1,21 @@
+import os
 import time
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient, WriteOptions
 
-DB_NAME = "deeppool"
-_client = None
+_client  = None
+_write_api = None
 
 def connect():
-    global _client
-    _client = InfluxDBClient(host='influxdb', port=8086)
+    global _client, _write_api
+    url    = os.environ["INFLUXDB_URL"]
+    token  = os.environ["INFLUXDB_TOKEN"]
+    org    = os.environ["INFLUXDB_ORG"]
+
     while True:
         try:
-            _client.create_database(DB_NAME)
-            _client.switch_database(DB_NAME)
+            _client = InfluxDBClient(url=url, token=token, org=org)
+            _client.ping()
+            _write_api = _client.write_api(write_options=WriteOptions(batch_size=1))
             print("[INFO] InfluxDB connected.")
             break
         except Exception as e:
@@ -18,13 +23,23 @@ def connect():
             time.sleep(10)
 
 def write(temp: float, pump_active: bool):
-    _client.write_points([
-        {
-            "measurement": "temperature_eau",
-            "fields": {"value": temp}
-        },
-        {
-            "measurement": "etat_pompe",
-            "fields": {"value": int(pump_active)}
-        }
-    ])
+    bucket = os.environ["INFLUXDB_BUCKET"]
+    org    = os.environ["INFLUXDB_ORG"]
+    _write_api.write(
+        bucket=bucket,
+        org=org,
+        record=[
+            {
+                "measurement": "temperature_eau",
+                "fields": {"value": temp}
+            },
+            {
+                "measurement": "etat_pompe",
+                "fields": {"value": int(pump_active)}
+            }
+        ]
+    )
+
+def close():
+    if _client:
+        _client.close()
