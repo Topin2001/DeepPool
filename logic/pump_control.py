69a48b79703b63b9a02cab46e6581b1e5496ee71
Topin2001 <wonodoou00@gmail.com>
2026-04-04 15:53:09 +0200

    added is_setup GPIO management and fix influxdb python library version

--- logic/pump_control.py
+++ logic/pump_control.py
@@ -2,16 +2,22 @@ import RPi.GPIO as GPIO
 import switch_reader
 
 GPIO_PIN = 17
+_is_setup = False
 
 def setup():
+    global _is_setup
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_PIN, GPIO.OUT)
     GPIO.output(GPIO_PIN, GPIO.LOW)
     switch_reader.setup()
+    _is_setup = True
 
 def set_pump(active: bool):
     GPIO.output(GPIO_PIN, GPIO.HIGH if active else GPIO.LOW)
     print(f"[PUMP] {'ON' if active else 'OFF'}")
 
 def cleanup():
-    GPIO.cleanup()
+    global _is_setup
+    if _is_setup:
+        GPIO.cleanup()
+        _is_setup = False
