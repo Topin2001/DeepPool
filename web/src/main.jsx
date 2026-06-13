d6d1e5e6ba95eac7e43fdf94fbcf73c2c0b4f523
Topin2001 <wonodoou00@gmail.com>
2026-06-13 19:31:29 +0200

    increase web refresh data frequence and format file

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { refetchInterval: 30000, retry: 1 }
-  }
-})
+    queries: { refetchInterval: 5000, retry: 1 },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientProvider>
-)
+  </QueryClientProvider>,
+);
